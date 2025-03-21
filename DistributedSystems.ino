f09adcf99756ff03c66294d1e546819deaac9493
unknown <filip.dymczyk@o2.pl>
2025-03-21 22:05:52 +0100

    FreeRTOS introduced.

--- DistributedSystems.ino
+++ DistributedSystems.ino
@@ -1,5 +1,6 @@
 #include <ArduinoMqttClient.h>
 #include <WiFiS3.h>
+#include <Arduino_FreeRTOS.h>
 #include "arduino_secrets.h"
 
 #define CONNECTION_DELAY_MS 1000u
@@ -7,6 +8,7 @@
 #define TEMPERATURE_IDX 0u
 #define INSULATION_IDX 1u
 #define WIND_IDX 2u
+#define THREAD_STACK_SIZE 512  
 
 WiFiClient wifi_client {};
 MqttClient mqttClient(wifi_client);
@@ -14,6 +16,7 @@ MqttClient mqttClient(wifi_client);
 static bool const retained = false;
 static int const publish_Qos = 1;
 static bool const dup = false;
+static TickType_t const mqtt_poll_delay = 100 / portTICK_PERIOD_MS;  // Convert 100ms to ticks.
 
 static float received_temperature = 0.0f;
 static float received_insulation = 0.0f;
@@ -21,56 +24,39 @@ static float received_wind  = 0.0f;
 static float sum_temperatures = 0.0f;
 static float sum_insulations = 0.0f;
 static float sum_winds  = 0.0f;
-
 static uint8_t data_count[] = {0u, 0u, 0u};
+
+SemaphoreHandle_t connection_semaphore;
 
 static void 
 on_message_received(int message_size);
+
+static void 
+mqtt_poll_task(void *pvParameters);
+
+static void 
+connect_task(void *pvParameters);
 
 void setup() 
 {
   Serial.begin(9600);
 
-  Serial.println("Attempting to connect to the Network...");
-  while (WiFi.begin(SSID, PASSWORD) != WL_CONNECTED) 
+  connection_semaphore = xSemaphoreCreateBinary();
+  
+  if (connection_semaphore == NULL) 
   {
-    Serial.print(".");
-    delay(CONNECTION_DELAY_MS);
+    Serial.println("Failed to create semaphore!");
+    return;
   }
 
-  Serial.println("You're connected to the Network!");
-  Serial.println();
+  xTaskCreate(connect_task, "WiFi Connect", THREAD_STACK_SIZE, NULL, 1, NULL);
+  xTaskCreate(mqtt_poll_task, "MQTT Polling", THREAD_STACK_SIZE, NULL, 1, NULL);
 
-  char const will_message[] = "Client unexpectedly disconnected!";
-  bool const will_retain = true;
-  int const will_Qos = 1;
-
-  mqttClient.beginWill(WILL_TOPIC, sizeof(will_message) / sizeof(char), will_retain , will_Qos);
-  mqttClient.print(will_message);
-  mqttClient.endWill();
-
-  Serial.println("Attempting to connect to the MQTT broker...");
-
-  while(!mqttClient.connect(BROKER, PORT))
-  {
-    Serial.print("MQTT connection failed! Error code = ");
-    Serial.println(mqttClient.connectError());
-    Serial.println("Retrying...");
-    delay(CONNECTION_DELAY_MS);
-  }
-
-  Serial.println("You're connected to the MQTT broker!");
-  Serial.println();
-
-  int const subscribe_Qos = 1;
-  mqttClient.subscribe(DATA_RECEIVE_TOPIC, subscribe_Qos);
-  mqttClient.onMessage(on_message_received);
+  vTaskStartScheduler();
 }
 
 void loop() 
-{
-  mqttClient.poll();
-}
+{}
 
 static void 
 on_message_received(int message_size)
@@ -104,21 +90,21 @@ on_message_received(int message_size)
   
   buff_temp[i] = '\0';
 
-  char output[100];
+  char message[100];
+
   switch(data_indicator)
   {
     case 't':
     {
       if(data_count[TEMPERATURE_IDX] < MAX_DATA_COUNT)
       {
-        received_temperature = atof(buff_temp);
+        received_temperature = strtof(buff_temp, nullptr);
         sum_temperatures += received_temperature;
         data_count[TEMPERATURE_IDX]++;
-        snprintf(output, sizeof(output), "Temperature: %f", received_temperature);
 
-        std::string message = "t" + std::to_string(received_temperature);
-        mqttClient.beginMessage(DATA_PUBLISH, message.length(), retained, publish_Qos, dup);
-        mqttClient.print(message.c_str());
+        uint16_t const len = snprintf(message, sizeof(message), "t%f", received_temperature);
+        mqttClient.beginMessage(DATA_PUBLISH, len, retained, publish_Qos, dup);
+        mqttClient.print(message);
         mqttClient.endMessage();
       }
       else
@@ -126,11 +112,10 @@ on_message_received(int message_size)
         float const temperature_mean = sum_temperatures / MAX_DATA_COUNT;
         sum_temperatures = 0.0f;
         data_count[TEMPERATURE_IDX] = 0u;
-        snprintf(output, sizeof(output), "Mean Temperature: %f", temperature_mean);
         
-        std::string message = "tm" + std::to_string(temperature_mean);
-        mqttClient.beginMessage(DATA_PUBLISH, message.length(), retained, publish_Qos, dup);
-        mqttClient.print(message.c_str());
+        uint16_t const len = snprintf(message, sizeof(message), "tm%f", temperature_mean);
+        mqttClient.beginMessage(DATA_PUBLISH, len, retained, publish_Qos, dup);
+        mqttClient.print(message);
         mqttClient.endMessage();
       }
       break;
@@ -139,17 +124,15 @@ on_message_received(int message_size)
     {
       if(data_count[INSULATION_IDX] < MAX_DATA_COUNT)
       {
-        received_insulation = atof(buff_temp);
+        received_insulation = strtof(buff_temp, nullptr);
         sum_insulations += received_insulation;
         data_count[INSULATION_IDX]++;
-        snprintf(output, sizeof(output), "Insulation: %f", received_insulation);
       }
       else
       {
         float const insulation_mean = sum_insulations / MAX_DATA_COUNT;
         sum_insulations = 0.0f;
         data_count[INSULATION_IDX] = 0u;
-        snprintf(output, sizeof(output), "Mean Insulation: %f", insulation_mean);
       }
       break;
     }
@@ -157,17 +140,15 @@ on_message_received(int message_size)
     {
       if(data_count[WIND_IDX] < MAX_DATA_COUNT)
       {
-        received_wind = atof(buff_temp);
+        received_wind = strtof(buff_temp, nullptr);
         sum_winds += received_wind;
         data_count[WIND_IDX]++;
-        snprintf(output, sizeof(output), "Wind: %f", received_wind);
       }
       else
       {
         float const wind_mean = sum_winds / MAX_DATA_COUNT;
         sum_winds = 0.0f;
         data_count[WIND_IDX] = 0u;
-        snprintf(output, sizeof(output), "Mean Wind: %f", wind_mean);
       }
       break;
     }
@@ -176,5 +157,56 @@ on_message_received(int message_size)
       break;
     }
   }
-  Serial.println(output);
+}
+
+
+static void 
+connect_task(void *pvParameters)
+{
+  Serial.println("Attempting to connect to the Network...");
+  while (WiFi.begin(SSID, PASSWORD) != WL_CONNECTED) 
+  {
+    Serial.print(".");
+    delay(CONNECTION_DELAY_MS);
+  }
+
+  Serial.println("You're connected to the Network!");
+  Serial.println();
+
+  delay(CONNECTION_DELAY_MS); // Delay to stabilize WiFi connection.
+
+  Serial.println("Attempting to connect the MQTT broker...");
+  while (!mqttClient.connect(BROKER, PORT)) 
+  {
+    Serial.print("MQTT connection failed! Error code = ");
+    Serial.println(mqttClient.connectError());
+    delay(CONNECTION_DELAY_MS);
+    Serial.println("Retrying...");
+  }
+
+  Serial.println("Connected to the MQTT broker!");
+
+  int const subscribe_Qos = 1;
+  mqttClient.subscribe(DATA_RECEIVE_TOPIC, subscribe_Qos);
+  mqttClient.onMessage(on_message_received);
+
+  xSemaphoreGive(connection_semaphore);
+  vTaskDelete(NULL);
+}
+
+static void 
+mqtt_poll_task(void *pvParameters)
+{
+  if(xSemaphoreTake(connection_semaphore, portMAX_DELAY) != pdTRUE)
+  {
+    return;
+  }
+
+  Serial.println("WiFi and MQTT connection complete. Starting MQTT polling...");
+
+  for(;;)
+  {
+    mqttClient.poll();
+    vTaskDelay(mqtt_poll_delay);
+  }
 }
