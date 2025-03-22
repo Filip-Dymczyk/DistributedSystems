920c76dc4d1f948dfd50fb8ac30fa32a33c8bb6c
unknown <filip.dymczyk@o2.pl>
2025-03-22 13:40:42 +0100

    Tasks for connection, parsing and data sending. Sempahore for callback and parsing synchronization.

--- DistributedSystems.ino
+++ DistributedSystems.ino
@@ -8,10 +8,10 @@
 #define TEMPERATURE_IDX 0u
 #define INSULATION_IDX 1u
 #define WIND_IDX 2u
-#define THREAD_STACK_SIZE 512
+#define THREAD_STACK_SIZE 256
 
 WiFiClient wifi_client {};
-MqttClient mqttClient(wifi_client);
+MqttClient mqtt_client(wifi_client);
 
 static bool const retained              = false;
 static int const publish_Qos            = 1;
@@ -26,16 +26,22 @@ static float sum_insulations      = 0.0f;
 static float sum_winds            = 0.0f;
 static uint8_t data_count[]       = {0u, 0u, 0u};
 
-SemaphoreHandle_t connection_semaphore;
+static char data_indicator;
+static char received_data[MAX_DATA_COUNT];
+
+SemaphoreHandle_t parse_data_semaphore = nullptr;
 
 static void
 on_message_received(int message_size);
 
 static void
+connect_task(void* pvParameters);
+
+static void
 mqtt_poll_task(void* pvParameters);
 
 static void
-connect_task(void* pvParameters);
+data_parse_task(void* pvParameters);
 
 static void
 data_send_task(void* pvParameters);
@@ -45,23 +51,17 @@ setup()
 {
     Serial.begin(9600);
 
-    connection_semaphore = xSemaphoreCreateBinary();
-
-    if(connection_semaphore == NULL)
+    parse_data_semaphore = xSemaphoreCreateBinary();
+    if(parse_data_semaphore == nullptr)
     {
         Serial.println("Failed to create semaphore!");
         return;
     }
 
-    xTaskCreate(connect_task, "WiFi Connect", THREAD_STACK_SIZE, NULL, 1, NULL);
-    xTaskCreate(mqtt_poll_task, "MQTT Polling", THREAD_STACK_SIZE, NULL, 1, NULL);
-
+    xTaskCreate(connect_task, "WiFi Connect", THREAD_STACK_SIZE, nullptr, 1, nullptr);
+    
+    Serial.println("Initialization complete!");
     vTaskStartScheduler();
-}
-
-void
-loop()
-{
 }
 
 static void
@@ -72,20 +72,18 @@ on_message_received(int message_size)
         return;
     }
 
-    char buff_temp[message_size];
     uint32_t i = 0;
 
-    char data_indicator = '\0';
-    while(mqttClient.available())
+    data_indicator = '\0';
+    while(mqtt_client.available())
     {
         if(data_indicator == '\0')
         {
-            data_indicator = (char)mqttClient.read();
+            data_indicator = static_cast<char>(mqtt_client.read());
         }
-
-        if(i < message_size - 1)
+        if(i < MAX_DATA_COUNT - 1)
         {
-            buff_temp[i] = (char)mqttClient.read();
+            received_data[i] = static_cast<char>(mqtt_client.read());
             i++;
         }
         else
@@ -93,25 +91,107 @@ on_message_received(int message_size)
             break;
         }
     }
+    received_data[i] = '\0';
 
-    buff_temp[i] = '\0';
+    xSemaphoreGive(parse_data_semaphore);  // Notify that parsing can happen.
+}
 
+static void
+connect_task(void* pvParameters)
+{
+    Serial.println("Attempting to connect to the Network...");
+    while(WiFi.begin(SSID, PASSWORD) != WL_CONNECTED)
+    {
+        Serial.print(".");
+        delay(CONNECTION_DELAY_MS);
+    }
+
+    Serial.println("You're connected to the Network!");
+
+    delay(CONNECTION_DELAY_MS);  // Delay to stabilize WiFi connection.
+
+    Serial.println("Attempting to connect the MQTT broker...");
+    while(!mqtt_client.connect(BROKER, PORT))
+    {
+        Serial.print("MQTT connection failed! Error code = ");
+        Serial.println(mqtt_client.connectError());
+        delay(CONNECTION_DELAY_MS);
+        Serial.println("Retrying...");
+    }
+
+    Serial.println("Connected to the MQTT broker!");
+
+    int const subscribe_Qos = 1;
+    mqtt_client.subscribe(DATA_RECEIVE_TOPIC, subscribe_Qos);
+    mqtt_client.onMessage(on_message_received);
+
+    delay(CONNECTION_DELAY_MS);  // Additional delay.
+
+    xTaskCreate(mqtt_poll_task, "MQTT Polling", THREAD_STACK_SIZE, nullptr, 1, nullptr);
+    xTaskCreate(data_parse_task, "Data Parsing", THREAD_STACK_SIZE, nullptr, 1, nullptr);
+    xTaskCreate(data_send_task, "Data Sending", THREAD_STACK_SIZE, nullptr, 1, nullptr);
+    vTaskDelete(NULL);
+}
+
+static void
+mqtt_poll_task(void* pvParameters)
+{
+    Serial.println("Starting MQTT polling...");
+
+    for(;;)
+    {
+        mqtt_client.poll();
+        vTaskDelay(mqtt_poll_delay);
+    }
+}
+
+static void
+parse_data();
+
+static void
+data_parse_task(void* pvParameters)
+{
+    Serial.println("Starting data parsing...");
+
+    for(;;)
+    {
+        if(xSemaphoreTake(parse_data_semaphore, portMAX_DELAY) == pdTRUE)
+        {
+            parse_data();
+        }
+        vTaskDelay(mqtt_poll_delay);
+    }
+}
+
+static void
+data_send_task(void* pvParameters)
+{
+    Serial.println("Starting data sending...");
+
+    for(;;)
+    {
+        vTaskDelay(mqtt_poll_delay);
+    }
+}
+
+static void
+parse_data()
+{
     char message[100];
-
     switch(data_indicator)
     {
         case 't':
         {
             if(data_count[TEMPERATURE_IDX] < MAX_DATA_COUNT)
             {
-                received_temperature = strtof(buff_temp, nullptr);
+                received_temperature = strtof(received_data, nullptr);
                 sum_temperatures += received_temperature;
                 data_count[TEMPERATURE_IDX]++;
 
                 uint16_t const len = snprintf(message, sizeof(message), "t%f", received_temperature);
-                mqttClient.beginMessage(DATA_PUBLISH, len, retained, publish_Qos, dup);
-                mqttClient.print(message);
-                mqttClient.endMessage();
+                mqtt_client.beginMessage(DATA_PUBLISH, len, retained, publish_Qos, dup);
+                mqtt_client.print(message);
+                mqtt_client.endMessage();
             }
             else
             {
@@ -120,9 +200,9 @@ on_message_received(int message_size)
                 data_count[TEMPERATURE_IDX]  = 0u;
 
                 uint16_t const len = snprintf(message, sizeof(message), "tm%f", temperature_mean);
-                mqttClient.beginMessage(DATA_PUBLISH, len, retained, publish_Qos, dup);
-                mqttClient.print(message);
-                mqttClient.endMessage();
+                mqtt_client.beginMessage(DATA_PUBLISH, len, retained, publish_Qos, dup);
+                mqtt_client.print(message);
+                mqtt_client.endMessage();
             }
             break;
         }
@@ -130,7 +210,7 @@ on_message_received(int message_size)
         {
             if(data_count[INSULATION_IDX] < MAX_DATA_COUNT)
             {
-                received_insulation = strtof(buff_temp, nullptr);
+                received_insulation = strtof(received_data, nullptr);
                 sum_insulations += received_insulation;
                 data_count[INSULATION_IDX]++;
             }
@@ -146,7 +226,7 @@ on_message_received(int message_size)
         {
             if(data_count[WIND_IDX] < MAX_DATA_COUNT)
             {
-                received_wind = strtof(buff_temp, nullptr);
+                received_wind = strtof(received_data, nullptr);
                 sum_winds += received_wind;
                 data_count[WIND_IDX]++;
             }
@@ -165,61 +245,7 @@ on_message_received(int message_size)
     }
 }
 
-static void
-connect_task(void* pvParameters)
+void
+loop()
 {
-    Serial.println("Attempting to connect to the Network...");
-    while(WiFi.begin(SSID, PASSWORD) != WL_CONNECTED)
-    {
-        Serial.print(".");
-        delay(CONNECTION_DELAY_MS);
-    }
-
-    Serial.println("You're connected to the Network!");
-    Serial.println();
-
-    delay(CONNECTION_DELAY_MS);  // Delay to stabilize WiFi connection.
-
-    Serial.println("Attempting to connect the MQTT broker...");
-    while(!mqttClient.connect(BROKER, PORT))
-    {
-        Serial.print("MQTT connection failed! Error code = ");
-        Serial.println(mqttClient.connectError());
-        delay(CONNECTION_DELAY_MS);
-        Serial.println("Retrying...");
-    }
-
-    Serial.println("Connected to the MQTT broker!");
-
-    int const subscribe_Qos = 1;
-    mqttClient.subscribe(DATA_RECEIVE_TOPIC, subscribe_Qos);
-    mqttClient.onMessage(on_message_received);
-
-    xSemaphoreGive(connection_semaphore);
-    vTaskDelete(NULL);
-}
-
-static void
-mqtt_poll_task(void* pvParameters)
-{
-    if(xSemaphoreTake(connection_semaphore, portMAX_DELAY) != pdTRUE)
-    {
-        return;
-    }
-
-    Serial.println("WiFi and MQTT connection complete. Starting MQTT polling...");
-
-    for(;;)
-    {
-        mqttClient.poll();
-        vTaskDelay(mqtt_poll_delay);
-    }
-}
-
-static void
-data_send_task(void* pvParameters)
-{
-    for(;;)
-    {
-    }
 }
